ff167440ea0637a90c5f61f0c498b243e337e925
animek12 <111511065+animek12@users.noreply.github.com>
2023-03-09 18:38:34 +0800

    Update config.js

--- config.js
+++ config.js
@@ -44,36 +44,36 @@ const readMore = more.repeat(4001)
 
 // kalo ga punya ketik "-" atau biarin aja biar ada creditsnya :v
 
-global.sgh = 'https://github.com/ZykoBotz-MD' // github
-global.sgc = 'https://chat.whatsapp.com/INq67rkTp8iAwiVvLOFAYH' // group whatsapp
+global.sgh = 'https://github.com/Raya2399' // github
+global.sgc = 'https://chat.whatsapp.com/BGyvKG4b0Qz2DnGXZTXuAX' // group whatsapp
 global.sdc = 'https://www.discord.com'//discord
-global.snh = 'https://youtu.be/E1Cb97Rb-8U' // my youtube channel
+global.snh = 'https://youtube.com/@alwanganzofficial7125' // my youtube channel
 global.sfb = 'https://www.facebook.com/' // facebook
-global.syt = 'https://www.youtube.com/@ZykoBotzMD'
-global.tik = 'https://www.tiktok.com/@zykosiapa' 
-global.tautanwa = 'https://wa.me/message/CXI3LHFLC6NON1'
-global.wame = 'https://wa.me/message/CXI3LHFLC6NON1' // kalau gk punya bisa ganti pake gini wa.me/6283133329293
+global.syt = 'https://www.youtube.com/@@alwanganzofficial7125'
+global.tik = 'https://www.tiktok.com/@alvin_ch1' 
+global.tautanwa = 'https://wa.me/6282261065144'
+global.wame = 'https://wa.me/6282261065144' // kalau gk punya bisa ganti pake gini wa.me/6283133329293
 
 /*============== PAYMENT ==============*/
-global.pdana = '0852 1264 9276' //dana
-global.povo = '0852 1264 9276' //ovo
-global.pgopay = '0852 1264 9276' //gopay
-global.plinkaja = '0852 1264 9276' //link aja
-global.ppulsa = '0852 1264 9276' //telkomsel
-global.ppulsa2 = '0831 3332 9293' // kalau ada kartu lain isi aja
-global.psaweria = '-'
+global.pdana = '0813 3299 5497' //dana
+global.povo = '-' //ovo
+global.pgopay = '0813 3299 5497' //gopay
+global.plinkaja = '-' //link aja
+global.ppulsa = '0813 3299 5497' //telkomsel
+global.ppulsa2 = '0812 5186 8708' // kalau ada kartu lain isi aja
+global.psaweria = 'https://saweria.co/Rimuru233'
 
 /*============== NOMOR ==============*/
-global.nomorbot = '6282124690625'
-global.nomorown = '6283133329293'
-global.nomorown1 = '6283133329293' //pake No Pacar lo Juga Boleh
-global.namebot = 'ZYKOBOTZ MD'
-global.nameown = 'ZYKO MD'
-global.nameown1 = 'LISA AMELIA' //Pake Nama Pacar Lo Juga Boleh
+global.nomorbot = '14842898416'
+global.nomorown = '6282261065144'
+global.nomorown1 = '6282261065144' //pake No Pacar lo Juga Boleh
+global.namebot = 'FIN MD'
+global.nameown = 'FIN OFC'
+global.nameown1 = 'FIN OFC' //Pake Nama Pacar Lo Juga Boleh
 
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ INFO BIO DATA OWN ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
-global.sig = '📷 ɪɴsᴛᴀɢʀᴀᴍ : https://instagram.com/kabulsaputra21' // ig lo
-global.sigpcr = '📷 ɪɴsᴛᴀɢʀᴀᴍ : https://instagram.com/lisaamelia8680' // ig cewek lo
+global.sig = '📷 ɪɴsᴛᴀɢʀᴀᴍ : https://instagram.com/al_vin.233' // ig lo
+global.sigpcr = '📷 ɪɴsᴛᴀɢʀᴀᴍ : https://instagram.com/al_vin.233' // ig cewek lo
 /*============== Github Thanks To ==============*/
 global.namagithub = 'Nama Github Belum Diisi' // nama github lu
 global.linkgithub = 'Link Githu Belum Diisi' // tarok link github lu
@@ -82,7 +82,7 @@ global.linkgithub = 'Link Githu Belum Diisi' // tarok link github lu
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['6283133329293', 'ZYKO MD', true],
+  ['6282261065144', 'FIN OFC', true],
   ['-', 'ISI KALAU ADA', true],
   ['-', 'ISI KALAU ADA', true],
   ['-', 'ISI KALAU ADA', true],
@@ -93,16 +93,16 @@ global.mods = []
 global.prems = [] 
 
 /*============== WATERMARK ==============*/
-global.gcname = 'ZYKOBOTZ MD'//nama group kalian
+global.gcname = 'FIN ANIME'//nama group kalian
 global.readMore = readMore
-global.wm = '                「 ZYKOBOTZ MD 」'
-global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘ZYKOBOTZ MD ˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
-global.wm3 = '⫹⫺ ZYKOBOTZ MD '
+global.wm = '                「 FIN MD 」'
+global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘FIN MD ˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
+global.wm3 = '⫹⫺ FIN MD '
 global.botdate = `⫹⫺ DATE: ${week} ${date}\n⫹⫺ 𝗧𝗶𝗺𝗲: ${wktuwib}`
 global.bottime = `T I M E : ${wktuwib}`
-global.titlebot = `⫹⫺ WhatsApp Bot | By ZYKO MD`
+global.titlebot = `⫹⫺ WhatsApp Bot | By FIN OFC`
 global.packname = '☂︎ ᴄʀᴇᴀᴛᴇᴅ ʙʏ'
-global.author = '              「 ZYKOBOTZ MD  あ」'
+global.author = '              「 FIN MD  あ」'
 global.week = `${week} ${date}`
 global.wibb = `${wktuwib}`
 global.danied = pickRandom(["✘ 𝗘𝗿𝗼𝗿 𝟰𝟬𝟰","𝘼𝙠𝙨𝙚𝙨 𝘿𝙞𝙩𝙤𝙡𝙖𝙠","⚠️ 𝘼𝙘𝙘𝙨𝙚𝙨 𝘿𝙖𝙣𝙞𝙚𝙙 ⚠️"])
@@ -112,8 +112,8 @@ global.stickpack = 'ZYKOBOTZ MD'
 global.stickauth = `By ©ZYKO MD`
  
 /*============== LOGO ==============*/
-global.fotonya1 = 'https://telegra.ph/file/bd078351110aaaaf3aa59.jpg' 
-global.fotonya2 = 'https://telegra.ph/file/bd078351110aaaaf3aa59.jpg'
+global.fotonya1 = 'https://telegra.ph/file/1ee2290eedd3330a66df8.jpg' 
+global.fotonya2 = 'https://telegra.ph/file/1ee2290eedd3330a66df8.jpg'
 global.logo = 'https://i.pinimg.com/564x/f7/d2/e4/f7d2e48fd59a8c01cd396bfc70b0a2d1.jpg'
 global.thumb = ImgEstetik() //Main Thumbnail
 global.imagebot = ImgMountain()
@@ -849,9 +849,9 @@ global.hoppai = ['https://telegra.ph/file/f34d76df4a2065af1a5ba.jpg','https://te
 
 global.flaaa = fla
 global.dtu = 'ɪɴꜱᴛᴀɢʀᴀᴍ'
-global.urlnya = "https://www.instagram.com/kabulsaputra21"
+global.urlnya = "https://www.instagram.com/al_vin.233"
 global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '0831-3332-9293'
+global.phn = '0822-6106-5144'
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ JNGN DI UBAH ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
 
 global.fsizedoc = '99999999999999'
